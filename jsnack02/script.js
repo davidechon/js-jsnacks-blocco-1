0a761bd70ab764069031cd00362d9c70f5e4cc4f
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-03-31 16:59:15 +0200

    jsnack02 flowchart

--- jsnack02/script.js
+++ jsnack02/script.js
@@ -5,3 +5,14 @@ alert("Hello World! 02");
 // Crea un array vuoto.
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
+
+//Flowchart
+// A --> B[INIZIO]
+// B --> C[Creo un array vuoto]
+// C --> D[chiedi di inserire un numero * 6 volte]
+// D --> E[confronta se il numero inserito è divisibile CON resto]
+// E -->|if| F{se numInserito === numDispari}
+// F -->|vero| G[Aggiungi numInserito all'array]
+// F -->|falso| H[Riprova]
+// G && H --> Z[FINE]
+
