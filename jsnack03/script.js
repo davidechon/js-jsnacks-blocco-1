06baf8f967dc0dcb8e40bfa285841f9c021fc09c
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-03-31 18:51:49 +0200

    jsnack03 wip

--- jsnack03/script.js
+++ jsnack03/script.js
@@ -14,3 +14,34 @@
 // E && F [stampa i risultati]
 // E --> Z[FINE]
 
+/////////////////
+// A --> B[INIZIO]
+// B --> C[Chiedi di inserire un numero di 4 cifre]
+let numUser = prompt('Inserisci un numero di 4 cifre');
+let contaNum;
+// C --> D{check se sono 4 cifre}
+if(numUser.length == 4){
+  console.log(numUser + " - " +  numUser.length);
+  // D -->|si| E[somma le tutte le cifre]
+  contaNum = numUser.spit("");
+
+}else{
+  // D -->|no| F[Inserimento non corretto. Riprova]
+  alert("Inserimento non corretto. Ricarica la pagina");
+}
+
+// E && F [stampa i risultati]
+
+// E --> Z[FINE]
+
+
+
+//////////////////////////////////////////////
+// let arrayVuoto = [];
+// for(let numDispari = 0; numDispari < 6; numDispari++){
+//  let numInserito = parseInt(prompt('Inserisci un numero da 1 a 10'));
+//   if(numInserito % 2 !== 0){
+//     arrayVuoto.push=(numInserito);
+//   }
+//   console.log(numDispari, numInserito, arrayVuoto);
+// }
