c40f8a261c9bc25f1ea1eec86d90f030f8aa93e6
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-03-31 16:26:06 +0200

    jsnack01 flowchart

--- jsnack01/script.js
+++ jsnack01/script.js
@@ -1,1 +1,19 @@
-alert("Hello World! 01");
+// alert("Hello World! 01");
+
+// Consegna
+// JSnack 1
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+//Flowchart
+// A --> B[INIZIO]
+// B --> C[Creo un array con i nomi degli invitati - 10 nomi]
+// C --> D[chiedi il nome utente]
+// D --> E[confronta il nome inserito con la lista invitati]
+// E -->|if| F{se nomeUtente === listaInvitati}
+// F -->|vero| G[nomeUtente può partecipare alla festa]
+// F -->|falso| H[Accesso negato]
+// G && H --> Z[FINE]
+
+const listaInvitati = ['Aurora', 'Davide', 'Ginevra', 'Lorenzo', 'Alice', 'Federico', 'Giorgia', 'GianMarco', 'Maddalena',  'Andrea',]
+
--- jsnack03/script.js
+++ jsnack03/script.js
@@ -1,1 +1,6 @@
 alert("Hello World! 03");
+
+//Consegna
+// JSnack 3
+// Chiedi un numero di 4 cifre all’utente
+// e calcola la somma di tutte le cifre che compongono il numero.
